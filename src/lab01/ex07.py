d1f3799545857ea34dfa8e1f385cd144ad8e0f64
Makar <applekosmos04@mail.ru>
2025-09-25 21:45:14 +0300

    asd

--- src/lab01/ex07.py
+++ src/lab01/ex07.py
@@ -1,24 +1,13 @@
-s = input().strip()
-
-ind=-1
-for i, char in enumerate(s):
-    if char.isupper():
-        ind= i
-        break
-step = 0
-for i in range(len(s)-1):
-    if s[i].isdigit() and s[i+1].isalpha():
-        step = (i + 1) - ind
-
-res=[]
-i=ind
-while i < len(s):
-    res.append(s[i])
-    if s[i] == '.':
-        break
-    i += step
-
-print(''.join(res)) 
-print(step)
+s=input('in: ')
+def stroka(s):
+    start = next(i for i, c in enumerate(s) if c.isupper())
+    step = (next(i for i, c in enumerate(s) if c.isdigit()) + 1) - start
+    result = ""
+    for i in range(start, len(s), step):
+        result += s[i]
+        if s[i] == ".":
+            break
+    return result
 
 
+print(f'out: {stroka(s)}')
--- src/lab01/qwr.py
+++
@@ -1,2 +0,0 @@
-a='thisisabracadabraH'
-print(len(a))
