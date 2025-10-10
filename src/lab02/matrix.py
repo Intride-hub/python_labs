28f60e462671ed19291ca931f2d007814ffd731a
Makar <applekosmos04@mail.ru>
2025-10-10 11:23:26 +0300

    sd

--- src/lab02/matrix.py
+++ src/lab02/matrix.py
@@ -26,16 +26,16 @@ def col_sums(mat: list[list[float | int]]) -> list[float]:
         return []
     return[sum(col) for col in zip(*mat)]
     
-#print(transpose([[1, 2, 3]]))     
-#print(transpose([[1], [2], [3]]))  
-#print(transpose([[1, 2], [3, 4]]))   
-#print(transpose([]))                
-#print(transpose([[1, 2], [3]]))          
+print(transpose([[1, 2, 3]]))     
+print(transpose([[1], [2], [3]]))  
+print(transpose([[1, 2], [3, 4]]))   
+print(transpose([]))                
+print(transpose([[1, 2], [3]]))          
 
-#print(row_sums([[1, 2, 3], [4, 5, 6]]))  
-#print(row_sums([[-1, 1], [10, -10]]))   
-#print(row_sums([[0, 0], [0, 0]]))    
-#print(row_sums([[1,2],[3]]))    
+print(row_sums([[1, 2, 3], [4, 5, 6]]))  
+print(row_sums([[-1, 1], [10, -10]]))   
+print(row_sums([[0, 0], [0, 0]]))    
+print(row_sums([[1,2],[3]]))    
 
 print(col_sums([[1, 2, 3], [4, 5, 6]]))  
 print(col_sums([[-1, 1], [10, -10]]))  
