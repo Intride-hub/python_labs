6c18e031c4f703a716ad4b2d1e1f6d04b3c48f73
Makar <applekosmos04@mail.ru>
2025-10-10 11:20:53 +0300

    sd

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -2,17 +2,13 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     if not nums:
         raise ValueError
     return (min(nums),max(nums))
-print(min_max([3, -1, 5, 5, 0]))
-print(min_max([42]))
-print(min_max([-5, -2, -9]))
-print(min_max([]))
-print(min_max([1.5, 2, 2.0, -3.1]))
+
 
 
 def unique_sorted(nums: list[float | int]) -> list[float | int]:
     a=set(nums)
     return sorted(a)
-print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+
 
 def flatten(mat: list[list | tuple]) -> list:
     res=[]
@@ -21,10 +17,23 @@ def flatten(mat: list[list | tuple]) -> list:
            raise TypeError
         res.extend(row)     
     return res
+
+# print(min_max([3, -1, 5, 5, 0]))
+# print(min_max([42]))
+# print(min_max([-5, -2, -9]))
+# print(min_max([1.5, 2, 2.0, -3.1]))
+# print(min_max([]))
+
+# print(unique_sorted([3, 1, 2, 1, 3]))
+# print(unique_sorted([-1, -1, 0, 2, 2]))
+# print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+# print(unique_sorted([]))
+
+
 print(flatten([[1, 2], [3, 4]]))
 print(flatten([[1, 2], (3, 4, 5)]))
 print(flatten([[1], [], [2, 3]]))
 print(flatten([[1, 2], "ab"]))
 
 
-    
+
