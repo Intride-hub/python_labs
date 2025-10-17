64640066d684c90e1e27197819ff934f10678130
Makar <applekosmos04@mail.ru>
2025-10-17 13:17:50 +0300

    asd

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -18,16 +18,16 @@ def flatten(mat: list[list | tuple]) -> list:
         res.extend(row)     
     return res
 
-# print(min_max([3, -1, 5, 5, 0]))
-# print(min_max([42]))
-# print(min_max([-5, -2, -9]))
-# print(min_max([1.5, 2, 2.0, -3.1]))
-# print(min_max([]))
+print(min_max([3, -1, 5, 5, 0]))
+print(min_max([42]))
+print(min_max([-5, -2, -9]))
+print(min_max([1.5, 2, 2.0, -3.1]))
+print(min_max([]))
 
-# print(unique_sorted([3, 1, 2, 1, 3]))
-# print(unique_sorted([-1, -1, 0, 2, 2]))
-# print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
-# print(unique_sorted([]))
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+print(unique_sorted([]))
 
 
 print(flatten([[1, 2], [3, 4]]))
---
+++ src/lab03/A.py
@@ -0,0 +1,70 @@
+def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
+    res=text
+    for char in ['\n','\r','\t']:
+        res=res.replace(char,' ')
+    
+    if yo2e:
+        res=res.replace('ё','е').replace('Ё','е')
+    
+    if casefold:
+        res=res.casefold()
+    res=' '.join(res.split())
+    assert normalize("ПрИвЕт\nМИр\t") == "привет мир"
+    assert normalize("ёжик, Ёлка") == "ежик, елка"
+
+    return res
+
+print(repr(normalize("ПрИвЕт\nМИр\t")))
+
+
+print(repr(normalize("ёжик, Ёлка")))
+
+
+print(repr(normalize("Hello\r\nWorld")))
+
+
+print(repr(normalize("  двойные   пробелы  ")))
+
+
+
+import re
+
+def tokenize(text: str) -> list[str]:
+    token = re.findall(r"[\w]+(?:-[\w]+)*", text)
+    return token
+
+print(tokenize("привет мир"))
+
+ 
+print(tokenize("hello,world!!!"))
+
+print(tokenize("по-настоящему круто"))
+
+print(tokenize("2025 год"))
+
+print(tokenize("emoji 😀 не слово"))
+
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    freq={}
+    for token in tokens:
+        if token in freq:
+            freq[token]+=1
+        else:
+            freq[token]=1
+    return freq
+
+print(count_freq(["a","b","a","c","b","a"]))
+
+print(count_freq(["bb","aa","bb","aa","cc"]))
+
+def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
+    sort_it=sorted(freq.items(), key=lambda x: (-x[1],x[0]))
+    return sort_it[:n]
+
+
+print(top_n(count_freq(["a","b","a","c","b","a"]),2))
+print(top_n(count_freq(["bb","aa","bb","aa","cc"]),2))
+
+
+
