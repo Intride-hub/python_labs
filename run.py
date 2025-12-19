5962c7443682ceffe755fa6b679ea4de437af3e8
Makar <applekosmos04@mail.ru>
2025-12-19 13:58:20 +0300

    sd

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from src.lab08.models import Student
 
 group = Group("data/lab09/students.csv")
 
-# CREATE
-group.add(Student("Чееловеек ПАУК", "2003-01-15", "БИВТ-21-1", 4.7))
+# # CREATE
+# group.add(Student("Чееловеек ПАУК", "2003-01-15", "БИВТ-21-1", 4.7))
 
 
 
@@ -16,18 +16,19 @@ group.add(Student("Чееловеек ПАУК", "2003-01-15", "БИВТ-21-1", 
 
 
 
-# # READ
+# # # READ
 # print(group.list())
 
 # # FIND
+
 # print(group.find("иванов"))
 
 # # UPDATE
-# group.update("Иванов Иван", gpa=4.6)
+#group.update("Иванов Иван", gpa=2.6)
 
 # # DELETE
-# group.remove("Петров Пётр")
+#group.remove("Чееловеек ПАУК")
 
 # # # STATS ★
-# # print(group.stats())
+print(group.stats())
 
